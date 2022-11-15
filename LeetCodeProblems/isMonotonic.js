c25fa32c03f0829f8c47dbc12e43bcbc6aed2e7e
Suneel Yadav <sunilyadav8898@gmail.com>
2022-11-15 23:17:30 +0530

    is monotonic array

---
+++ LeetCodeProblems/isMonotonic.js
@@ -0,0 +1,12 @@
+function isMonotonic(arr) {
+    let isDec = true
+    let isInc = true
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) isInc = false
+    }
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] < arr[j + 1]) isDec = false
+    }
+    return isDec || isInc
+}
+console.log("isMonotonic", isMonotonic([6, 5,2, 4, 4]));
---
+++ LeetCodeProblems/longestPalindrome.js
@@ -0,0 +1,6 @@
+var longestPalindrome = function (s) {
+   let count=0
+   let i=0
+   while()
+}
+console.log("longestPalindrome", longestPalindrome("abc"));
