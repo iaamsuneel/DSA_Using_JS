16ecc72a2bb4f54feb789e08580fd8dcd64c8315
suneelvelocis <suneel.yadav@velocis.co.in>
2023-06-10 13:05:21 +0530

    Bubble Sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,21 +1,18 @@
 function bubbleSort(arr) {
-    let n = arr.length
-    function swap(arr, x, y) {
-        console.log("swap", arr[x], arr[y])
-        var temp = arr[x]
-        arr[x] = arr[y]
-        arr[y] = temp
+  let n = arr.length;
+  function swap(arr, x, y) {
+    var temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+  }
+  for (var i = 0; i < n - 1; i++) {
+    for (var j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
     }
-    for (var i = 0; i < n - 1; i++) {
-        console.log("firstLoop", arr[i])
-        for (var j = 0; j < n - i - 1; j++) {
-            console.log("secondLoop", arr[j], arr[j + 1])
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1)
-            }
-        }
-
-    }
-    return arr;
+  }
+  return arr;
 }
-console.log(bubbleSort([24, 18, 38, 43, 14, 40, 1, 54]))
+console.log(bubbleSort([24, 18, 38, 43, 14, 40, 1, 54]));
+// output => [1, 14, 18, 24,38, 40, 43, 54 ]
