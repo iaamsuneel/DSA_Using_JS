79da5666d3d2ab450627aca10ab6626f2d3a7dee
Suneel Yadav <Sunilyadav8898@gmail.com>
2024-09-17 12:42:01 +0530

    reverse string using two pointer

---
+++ Technique/TwoPointer/Reverese.js
@@ -0,0 +1,12 @@
+function stringReverse(str) {
+  const s = str.split("");
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
+  return s.join("");
+}
+console.log("Reverse String ::: ", stringReverse("NewList"));
