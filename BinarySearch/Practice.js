948be7a11dc0573b1fad3949a6da4e3a753afd55
Suneel Yadav <Sunilyadav8898@gmail.com>
2025-07-22 16:36:00 +0530

    new content

---
+++ BinarySearch/Practice.js
@@ -0,0 +1,58 @@
+/*  Binary Search is a searching algorithm for searching an element in a sorted list or array. 
+Binary Search is efficient than Linear Search algorithm and
+ performs the search operation in logarithmic time complexity for sorted arrays or lists.
+Binary Search performs the search operation by repeatedly dividing the search interval in half.
+The idea is to begin with an interval covering the whole array.
+If the value of the search key is less than the item in the middle of the interval,
+    narrow the interval to the lower half.
+ Otherwise narrow it to the upper half.
+ Repeatedly check until the value is found or the interval is empty.
+ 
+ Time Complexity:  O(Log N), where N is the number of elements in the array.
+ */
+function binarySearch(arr, key) {
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+  // for mid index
+  let mid = Math.floor(startIndex + (endIndex - startIndex) / 2);
+  while (startIndex <= endIndex) {
+    if (arr[mid] === key) {
+      // console.log("mid")
+      return mid;
+    }
+    // go to right
+    else if (key > arr[mid]) {
+      startIndex = mid + 1;
+      // console.log("mid+1")
+    } else {
+      // go to left
+      endIndex = mid - 1;
+      // console.log("mid-1")
+    }
+    mid = Math.floor(startIndex + (endIndex - startIndex) / 2);
+  }
+  return -1;
+}
+console.log(binarySearch([2, 4, 6, 7, 9, 11, 27], 11));
+console.log(binarySearch([2, 4, 6, 7, 9, 11, 27, 17, 19], 4));
+// find index of target element
+// Problem 1:
+function searchElement(arr, key) {
+  let starInd = 0,
+    endInd = arr.length - 1;
+  let midIndex = Math.floor(starInd + (endInd - starInd) / 2);
+  while (starInd <= endInd) {
+    if (arr[midIndex] === key) {
+      return midIndex;
+    } else if (key > arr[midIndex]) {
+      starInd = midIndex + 1;
+    } else {
+      starInd = midIndex - 1;
+    }
+    midIndex = Math.floor(starInd + (endInd - starInd) / 2);
+  }
+  return -1;
+}
+console.time("searchElement");
+console.log(searchElement([2, 4, 6, 7, 9, 11, 27], 11));
+console.timeEnd("searchElement");
